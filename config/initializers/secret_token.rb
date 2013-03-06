5c91dbe1eafeb16cd59e2f39c3be195a4c2ba437
Ross Wilson <me@rosswilson.co.uk>
2013-03-06 04:41:08 +0000

    Initial commit, blank Rails app with custom README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EduCam::Application.config.secret_token = '9628cebacd3d6206ac677a0dc28a9c54a3bf53ab91dbaad5fffe9b20638d0b65d72a56f7aa54525e179ccb37e75e7639031ff0a09c94a1de746717ef38b65236'
