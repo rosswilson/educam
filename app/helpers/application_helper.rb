fb6a315d7d67a9e5f2cafce567da5ca94c8acf02
Ross Wilson <me@rosswilson.co.uk>
2013-03-06 04:49:25 +0000

    Added layout code and removed scaffold CSS file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+ def title(page_title)
+    content_for(:title) { page_title }
+    page_title.chomp!
+  end
+
+  def sidebar(&content)
+    content_for(:sidebar) { yield(content) }
+  end 
 end
